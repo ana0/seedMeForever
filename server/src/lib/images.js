3318813096447f0e1bb0cca0811bc8a52730fe82
anon <sarah.anne.friend@gmail.com>
2019-06-26 10:36:25 -0400

    Lowercase

--- server/src/lib/images.js
+++ server/src/lib/images.js
@@ -2,7 +2,8 @@ const imageFilter = function (req, file, cb) {
   // accept image only
   console.log(req)
   console.log(file)
-  if (!file.originalname.match(/\.(jpg|jpeg|heic|png|gif)$/)) {
+  const lower = file.originalname.toLowerCase()
+  if (!lower.match(/\.(jpg|jpeg|heic|png|gif)$/)) {
       return cb(new Error('Only image files are allowed!'), false);
   }
   cb(null, true);
