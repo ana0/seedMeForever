da501a6478f2c64efc408e6e29da00ea0370be53
anon <sarah.anne.friend@gmail.com>
2019-06-26 10:25:47 -0400

    heic

--- server/src/lib/images.js
+++ server/src/lib/images.js
@@ -2,7 +2,7 @@ const imageFilter = function (req, file, cb) {
   // accept image only
   console.log(req)
   console.log(file)
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+  if (!file.originalname.match(/\.(jpg|jpeg|heic|png|gif)$/)) {
       return cb(new Error('Only image files are allowed!'), false);
   }
   cb(null, true);
