1dc93c8e624df0088d9b43c4a903e29606f83a7c
anon <sarah.anne.friend@gmail.com>
2019-08-22 19:37:52 -0400

    Phrasing 2

--- client/src/components/ConsentForm.js
+++ client/src/components/ConsentForm.js
@@ -8,7 +8,7 @@ class ConsentForm extends Component {
       instructions: "This is a memorial to the 41,415 plant and animal species" +
         "currently classified as endangered or threated" +
         ". This makes up 27% of all assessed species: 40% of amphibians, 25% of " +
-        "mammals, 14% of birds, and 70% of assessed plants. Early this May, the United Nations " +
+        "mammals, 14% of birds, and 70% of plants. Early this May, the United Nations " +
         "released a report on global biodiversity loss, finding that it is in unprecedented " +
         "decline as a result of human activities.\n\nWould you like to contribute?",
     };
