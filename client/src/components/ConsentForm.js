d5a5e7372c22e54fae7ad1a036fa6fcf61e682e9
anon <sarah.anne.friend@gmail.com>
2019-08-22 19:37:20 -0400

    Phrasing

--- client/src/components/ConsentForm.js
+++ client/src/components/ConsentForm.js
@@ -5,8 +5,9 @@ class ConsentForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      instructions: "This is a memorial to the 41,415 endangered and threated species" +
-        ". This makes up 27% of all assessed species, 40% of amphibians, 25% of " +
+      instructions: "This is a memorial to the 41,415 plant and animal species" +
+        "currently classified as endangered or threated" +
+        ". This makes up 27% of all assessed species: 40% of amphibians, 25% of " +
         "mammals, 14% of birds, and 70% of assessed plants. Early this May, the United Nations " +
         "released a report on global biodiversity loss, finding that it is in unprecedented " +
         "decline as a result of human activities.\n\nWould you like to contribute?",
