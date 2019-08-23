37511c056c1901391cd59eb9641e28cef1ba2d0a
anon <sarah.anne.friend@gmail.com>
2019-08-23 07:22:23 -0400

    Typo

--- client/src/components/ConsentForm.js
+++ client/src/components/ConsentForm.js
@@ -5,7 +5,7 @@ class ConsentForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      instructions: "This is a memorial to the 41,415 plant and animal species" +
+      instructions: "This is a memorial to the 41,415 plant and animal species " +
         "currently classified as endangered or threated" +
         ". This makes up 27% of all assessed species: 40% of amphibians, 25% of " +
         "mammals, 14% of birds, and 70% of plants. Early this May, the United Nations " +
